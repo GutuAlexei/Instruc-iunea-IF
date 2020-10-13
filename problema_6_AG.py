12c92bb201f54c4e7d68582fdd968f9d44b55737
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2020-10-13 14:58:27 +0300

    Delete problema_6_AG.py

--- problema_6_AG.py
+++
@@ -1,8 +0,0 @@
-a=int(input('intr. primul numar='))
-b=int(input('intr.al doilea numar='))
-if a+1==b:
-   print(a, ' este predecesorul lui', b)
-elif a-1==b:
-   print(a, 'este succesorul lui', b) 
-else:
-   print(a, ' nu este nici predecesorul și nici succesorul lui', b)   
