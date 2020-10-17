1e8b5d9a53608c222760dc2bfa92a206759ac862
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2020-10-18 00:36:01 +0300

    Delete problema_4_AG.py

--- problema_4_AG.py
+++
@@ -1,8 +0,0 @@
-a=int(input('intr. primul numar='))
-b=int(input('intr.al doilea numar='))
-c=int(input('intr. al treilea numar='))
-S=int(input('intr. suma numerilor='))
-if ((a+b==S) or (a+c==S) or (b+c==S)):
-   print('Numarul ramas in cutie este:' , (a+b+c)-S)
-else:
-   print('error')   
