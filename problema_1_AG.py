7852f67ff8d5a674650349a1a954899727957f78
GutuAlexei <70639790+GutuAlexei@users.noreply.github.com>
2020-10-18 00:35:31 +0300

    Delete problema_1_AG.py

--- problema_1_AG.py
+++
@@ -1,8 +0,0 @@
-a=int(input('intr. primul numar='))
-b=int(input('intr.al doilea numar='))
-if a>b:
-   print('cel mai mic numar este' , b) 
-elif a<b:
-   print('cel mai mic numar este' , a) 
-else :
-   print('Numerele sunt egale')  
